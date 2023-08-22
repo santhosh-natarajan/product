f2a3dd7109c7dbf8b343378dc84b07502d66239a
santhosh-natarajan <santhosh.swengineer@gmail.com>
2023-08-22 22:56:29 +0530

    Grid layout updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Grid } from "@mui/material";
 import { Footer } from "./Components/Footer.Component";
 import { Header } from "./Components/Header.Component";
+import { } from "@mui/material/Grid";
 
 function App() {
   return (
-    <div>
-        <Header />
-        <Footer/>
-    </div>
+    // <div>
+    //     <Header />
+    //     <Footer/>
+    // </div>
+    <Grid container>
+        <Grid xs={8}>List view</Grid>
+        <Grid xs={4}>Form view</Grid>
+    </Grid>
   );
 }
-
+  
 export default App;
